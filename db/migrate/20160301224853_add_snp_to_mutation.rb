1e657d9177f3a503e3f5da21f664e46476c82b39
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2016-06-23 12:32:47 +0100

    Transformed snp to lowercase

--- db/migrate/20160301224853_add_snp_to_mutation.rb
+++ db/migrate/20160301224853_add_snp_to_mutation.rb
@@ -1,6 +1,6 @@
 class AddSnpToMutation < ActiveRecord::Migration
   def change
-    add_reference :mutations, :SNP, index: true
+    add_reference :mutations, :snp, index: true
     add_foreign_key :mutations, :SNPs
   end
 end
