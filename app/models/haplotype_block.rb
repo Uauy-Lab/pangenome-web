665e73154f443a19d2f80a4bceab8e85c7d1ffae
Ricardo H. Ramirez-Gonzalez <homo.nec.loco@gmail.com>
2019-12-23 11:40:35 -0600

    Column to ignore genes without mapping

--- app/models/haplotype_block.rb
+++ app/models/haplotype_block.rb
@@ -3,7 +3,7 @@ class HaplotypeBlock < ApplicationRecord
   has_one :scaffold, through: :region
   belongs_to :assembly
   belongs_to :haplotype_set
-  belongs_to :first_feature, class_name: :Feature, foreign_key: :first_feature
-  belongs_to :last_feature,  class_name: :Feature, foreign_key: :last_feature
+  belongs_to :first_feature, class_name: :Feature, foreign_key: :first_feature, optional: true
+  belongs_to :last_feature,  class_name: :Feature, foreign_key: :last_feature, optional: true
   belongs_to :reference_assembly,  class_name: :Assembly, foreign_key: :reference_assembly
 end
