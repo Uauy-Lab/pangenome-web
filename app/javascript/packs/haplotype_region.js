ac5282d93d2f9540eaf6f66e27a20bd5f2432216
Ricardo H Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2020-04-08 17:52:36 +0100

    Refactored the structure of the class to have common functions for different region types

--- app/javascript/packs/haplotype_region.js
+++ app/javascript/packs/haplotype_region.js
@@ -1,48 +1,53 @@
+class Region{
+	constructor(values){
+		this.assembly     = values.assembly;
+		this.reference    = values.reference;
+		this.chromosome   = values.chromosome;
+		this.start        = parseInt( values.start);
+		this.end          = parseInt(values.end);
+	};
+
+	length(){
+		return this.end - this.start
+	}
+	overlap(other){
+		if(other == null){
+			return false;
+		}
+		if(other.reference != this.reference){
+			return false;
+		}
+		if(other.chromosome != this.chromosome){
+			return false;
+		}
+		var left  = other.start >= this.start && other.start <= this.end;
+		var rigth = this.start >= other.start && this.start <= other.end;
+		return  left || rigth; 
+	};
+
+	contains(other){
+		if(other.assembly != this.assembly){
+			return false;
+		}
+		if(other.chromosome != this.chromosome){
+			return false;
+		}
+		return other.start >= this.start && other.end <= this.end;
+	};
 
 
-var HaplotypeRegion = function(values){
-	this.assembly     = values.assembly;
-	this.reference    = values.reference;
-	this.chromosome   = values.chromosome;
-	this.start        = parseInt( values.start);
-	this.end          = parseInt(values.end);
-	this.block_no     = parseInt(values.block_no);
-	this.chr_length   = parseInt(values.chr_length);
-	this.merged_block = 0;
+	region_string(){
+		return "" + this.assembly +":\t" + this.chromosome + ":\t" + this.start + "-\t"  +this.end;
+	}
 };
 
-HaplotypeRegion.prototype.length = function(){
-	return this.end - this.start
+class HaplotypeRegion extends Region{
+	constructor(values){
+		super(values);
+		this.block_no     = parseInt(values.block_no);
+		this.chr_length   = parseInt(values.chr_length);
+		this.merged_block = 0;	
+	};
 };
-
-HaplotypeRegion.prototype.overlap = function(other){
-	if(other == null){
-		return false;
-	}
-	if(other.assembly != this.assembly){
-		return false;
-	}
-	if(other.chromosome != this.chromosome){
-		return false;
-	}
-	var left  = other.start >= this.start && other.start <= this.end;
-	var rigth = this.start >= other.start && this.start <= other.end;
-	return  left || rigth; 
-};
-
-HaplotypeRegion.prototype.contains = function(other){
-	if(other.assembly != this.assembly){
-		return false;
-	}
-	if(other.chromosome != this.chromosome){
-		return false;
-	}
-	return other.start >= this.start && other.end <= this.end;
-
-};
-
-HaplotypeRegion.prototype.region_string = function(){
-	return "" + this.assembly +":\t" + this.chromosome + ":\t" + this.start + "-\t"  +this.end;
-}
-
 window.HaplotypeRegion = HaplotypeRegion;
+window.Region = Region;
