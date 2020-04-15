09c4b7666d387837404d017b430e2e13a8708f4a
Ricardo H Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2020-04-15 10:31:21 +0100

    Refectoring to reduce complexity of constructor

--- app/javascript/packs/haplotype_drag_axis.js
+++ app/javascript/packs/haplotype_drag_axis.js
@@ -4,64 +4,40 @@ class DragAxis extends RegionAxis{
 		super(svg_g, scale,target, status);
 		var self = this;
 		this.bar_properties = {x: 0, y: -8, width: this._width(), height: 16, dragbarw:2}
-		
 		var drag = d3.drag()
-		.on("drag", function () {
-         	self.dragmove(this);})
-		.on("end", function(){
-		 	self.update_target_coordinates(target); 	
-		});
-
+			.on("drag",  () =>	self.dragmove(this))
+			.on("end", ()   => self.update_target_coordinates(target));
 		var dragleft = d3.drag()
-		.on("drag", function () {
-         	self.drag_resize_left(this);})
-		.on("end", function(){
-		 	self.update_target_coordinates(target); 	
-		});
-
+			.on("drag", () => self.drag_resize_left(this))
+			.on("end", () => self.update_target_coordinates(target));
 		var dragrigth = d3.drag()
-		.on("drag", function () {
-         	self.drag_resize_right(this);})
-		.on("end", function(){
-		 	self.update_target_coordinates(target); 	
-		});
-
+			.on("drag", () => self.drag_resize_right(this))
+			.on("end", () =>  self.update_target_coordinates(target));
       	var newg = this.svg_g.append("g")
       	this.dragrect = newg.append("rect").data([this.bar_properties])
-      	.attr("fill-opacity", .5)
-    	.attr("fill", "lightgray")
-      	.attr("cursor", "move")
-      	.call(drag);
-     
-      	var d = this.bar_properties;
-      	this.dragbarleft = newg.append("rect").data([this.bar_properties])
-	    .attr("y",   d.y + (d.dragbarw/2) )
-	    .attr("height", d.height - d.dragbarw)
-	    .attr("width", d.dragbarw)
-	    .attr("fill", "darkgray")
-	    .attr("fill-opacity", 1)
-	    .attr("cursor", "ew-resize")
-	    .call(dragleft);
-
-	    this.dragbarright = newg.append("rect").data([this.bar_properties])
-	    .attr("y",   d.y + (d.dragbarw/2) )
-	    .attr("height", d.height - d.dragbarw)
-	    .attr("width", d.dragbarw)
-	    .attr("fill", "darkgray")
-	    .attr("fill-opacity", 1)
-	    .attr("cursor", "ew-resize") 
-	    .call(dragrigth);
-
+	      	.attr("fill-opacity", .5)
+	    	.attr("fill", "lightgray")
+	      	.attr("cursor", "move")
+	      	.call(drag);     
+      	this.dragbarleft  = this.newDragBar(newg,dragleft);
+	    this.dragbarright = this.newDragBar(newg,dragrigth);
 	    this.refresh_range(0);
+	}
 
+	newDragBar(newg, callback){
+		return newg.append("rect").data([this.bar_properties])
+	    .attr("y",   d   => d.y + (d.dragbarw/2) )
+	    .attr("height",d =>  d.height - d.dragbarw)
+	    .attr("width", d =>d.dragbarw)
+	    .attr("fill", "darkgray")
+	    .attr("fill-opacity", 1)
+	    .attr("cursor", "ew-resize").call(callback)
 	}
 
 	drag_resize_left(d){
 		var _drag ;
 		var bp = this.bar_properties;
-		this.dragbarleft.each(function(d2, i){
-			_drag = d2.x;
-		});
+		this.dragbarleft.each(d2 => _drag = d2.x);
 		var new_x = Math.max(0, Math.min(_drag + bp.width - (bp.dragbarw / 2), d3.event.x)); 
      	var width = bp.width + (_drag - new_x);
         bp.width = width;
@@ -73,9 +49,7 @@ class DragAxis extends RegionAxis{
 	drag_resize_right(d){
 		var _drag ;	
 		var bp = this.bar_properties;
-		this.dragbarright.each(function(d2, i){
-			_drag = d2.width;
-		});
+		this.dragbarright.each(d2 => _drag = d2.width);
 		var largest_width = this._width() - bp.x - bp.dragbarw / 2
 		var width = d3.event.dx +  _drag;
 		width     = Math.max(5,Math.min(width, largest_width)); 
