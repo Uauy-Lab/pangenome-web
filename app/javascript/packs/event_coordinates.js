5ccd57b4d45b0b73e2598ebcef586bf992dc7339
Ricardo H Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2021-04-07 14:08:21 +0100

    Class that contains the event_coordiantes

---
+++ app/javascript/packs/event_coordinates.js
@@ -0,0 +1,27 @@
+class EventCoordinates{
+	#coords;
+	constructor(){
+		this.hash         = "";
+		this.asm          = undefined;
+		this.blocks       = [];
+		this.#coords       = [0,0];
+		this.in_plot      = false;
+		this.in_y_axis    = false;
+		this.in_score     = false;
+		this.in_haplotype = false;
+	}
+
+	get x(){
+		return this.#coords[0];
+	}
+
+	get y(){
+		return this.#coords[1];
+	}
+
+	set coords(c){
+		this.#coords = c;
+	}
+}
+
+window.EventCoordinates = EventCoordinates;
