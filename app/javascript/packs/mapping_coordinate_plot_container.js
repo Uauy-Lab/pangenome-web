82951fb497b22daaf0d15f71573a130cfe097733
Ricardo H. Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2021-09-07 20:48:48 +0100

    Formatting and added <g> attributes for mapping coordiante

--- app/javascript/packs/mapping_coordinate_plot_container.js
+++ app/javascript/packs/mapping_coordinate_plot_container.js
@@ -1,6 +1,12 @@
 class MappingCoordinatePlotContainer extends PlotContainer{
+	#g_axis;
+	#g_mapping;
+	#g_mapped_blocks;
 	constructor(svg_g,width,height,offset_x,offset_y, current_status){
 		super(svg_g,width,height,offset_x,offset_y, current_status);
+
+
 	}
+
 }
 window.MappingCoordinatePlotContainer = MappingCoordinatePlotContainer;
